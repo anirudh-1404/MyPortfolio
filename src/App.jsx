999584ca5e796523d84eadd86783a063ca7c2e7e
Anirudh Joshi <anirudhj545@gmail.com>
2025-12-25 02:04:41 +0530

    basic setup

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+};
+
+export default App;
